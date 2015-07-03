a8545cffa16c60e6a9f53f867e5b54266b8a89a7
Shichang Xu <xsc@umich.edu>
2015-07-03 11:03:13 -0400

    Fix bug of poping multiple authenticate window

--- Mobilyzer/src/com/mobilyzer/AccountSelector.java
+++ Mobilyzer/src/com/mobilyzer/AccountSelector.java
@@ -220,7 +220,7 @@ public class AccountSelector {
         // User input required. (A UI will pop up for user's consent to allow
         // this app access account information.)
         Logger.i("Starting account manager activity");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
       } else {
         Logger.i("Executing getCookie task");
