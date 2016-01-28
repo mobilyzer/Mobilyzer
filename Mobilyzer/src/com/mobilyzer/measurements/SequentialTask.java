58457a149200c88c90624bdfa0c643578ea411e2
Ashkan Nikravesh <ashkannikravesh@gmail.com>
2016-01-28 11:59:14 -0500

    supporting server side scheduling of sequential tasks

--- Mobilyzer/src/com/mobilyzer/measurements/SequentialTask.java
+++ Mobilyzer/src/com/mobilyzer/measurements/SequentialTask.java
@@ -14,6 +14,7 @@
 package com.mobilyzer.measurements;
 
 import java.io.InvalidClassException;
+import java.lang.reflect.Type;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,12 +27,21 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.myjson.Gson;
+import com.google.myjson.reflect.TypeToken;
 import com.mobilyzer.Config;
 import com.mobilyzer.MeasurementDesc;
 import com.mobilyzer.MeasurementResult;
 import com.mobilyzer.MeasurementTask;
 import com.mobilyzer.exceptions.MeasurementError;
+import com.mobilyzer.measurements.TracerouteTask.TracerouteDesc;
 import com.mobilyzer.util.Logger;
+import com.mobilyzer.util.MeasurementJsonConvertor;
+import com.mobilyzer.util.PhoneUtils;
 
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -56,20 +66,45 @@ public class SequentialTask extends MeasurementTask{
   private long duration;
   private volatile MeasurementTask currentTask;
 
-  public static class SequentialDesc extends MeasurementDesc {     
-
+  public static class SequentialDesc extends MeasurementDesc { 
+	  
+	public ArrayList<MeasurementTask> subTasks;
     public SequentialDesc(String key, Date startTime,
         Date endTime, double intervalSec, long count, long priority,
         int contextIntervalSec, Map<String, String> params)
             throws InvalidParameterException {
       super(SequentialTask.TYPE, key, startTime, endTime, intervalSec, count,
-        priority, contextIntervalSec, params);  
-      //      initializeParams(params);
+        priority, contextIntervalSec, params);
+            initializeParams(params);
+            if (subTasks==null || subTasks.size()==0){
+            	throw new InvalidParameterException("Sequential task must contain at-least one sub task");
+            }
 
     }
 
     @Override
     protected void initializeParams(Map<String, String> params) {
+        if (params == null) {
+            return;
+        }
+        subTasks=new ArrayList<MeasurementTask>();
+        String tasksJsonList = null;
+        if ((tasksJsonList = params.get("tasks")) != null && tasksJsonList.length() > 0){
+        	try {
+				JSONArray jsonArray = new JSONArray(tasksJsonList);
+				for (int i = 0; i < jsonArray.length(); i++) {
+					JSONObject jsonObj = jsonArray.optJSONObject(i);
+					if (jsonObj != null &&  MeasurementTask.getMeasurementTypes().contains(jsonObj.get("type"))) {
+						MeasurementTask subTask = MeasurementJsonConvertor.makeMeasurementTaskFromJson(jsonObj);
+						subTasks.add(subTask);
+					}
+				}
+				
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+        }
     }
 
     @Override
@@ -77,8 +112,15 @@ public class SequentialTask extends MeasurementTask{
       return SequentialTask.TYPE;
     }  
     
+    @Override
+    public int describeContents() {
+      return 0;
+    }
+    
     protected SequentialDesc(Parcel in) {
       super(in);
+//      ClassLoader loader = Thread.currentThread().getContextClassLoader();
+      subTasks = in.readArrayList(MeasurementTask.class.getClassLoader());
     }
 
     public static final Parcelable.Creator<SequentialDesc> CREATOR
@@ -91,6 +133,13 @@ public class SequentialTask extends MeasurementTask{
         return new SequentialDesc[size];
       }
     };
+    
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+      super.writeToParcel(dest, flags);
+      dest.writeList(subTasks);
+    }
+    
   }
 
   @SuppressWarnings("rawtypes")
@@ -98,7 +147,21 @@ public class SequentialTask extends MeasurementTask{
     return SequentialDesc.class;
   }
 
-
+  public SequentialTask(MeasurementDesc desc) {
+	    super(new SequentialDesc(desc.key, desc.startTime, desc.endTime,
+	      desc.intervalSec, desc.count, desc.priority, desc.contextIntervalSec,
+	      desc.parameters));
+	    //we are using executor because it has builtin support for execution timeouts.
+	    executor=Executors.newSingleThreadExecutor();
+	    long totalduration=0;
+	    this.tasks=(List<MeasurementTask>)(((SequentialDesc)getDescription()).subTasks).clone();
+	    for(MeasurementTask mt: tasks){
+	      totalduration+=mt.getDuration();
+	    }
+	    this.duration=totalduration;
+	    this.stopFlag=false;
+	    this.currentTask=null;
+  }
 
   public SequentialTask(MeasurementDesc desc, ArrayList<MeasurementTask> tasks) {
     super(new SequentialDesc(desc.key, desc.startTime, desc.endTime,
@@ -118,9 +181,9 @@ public class SequentialTask extends MeasurementTask{
   
   protected SequentialTask(Parcel in) {
     super(in);
-    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+//    ClassLoader loader = Thread.currentThread().getContextClassLoader();
     // we cannot directly cast Parcelable[] to MeasurementTask[]. Cast them one-by-one
-    Parcelable[] tempTasks = in.readParcelableArray(loader);
+    Parcelable[] tempTasks = in.readParcelableArray(MeasurementTask.class.getClassLoader());
     executor=Executors.newSingleThreadExecutor();
     tasks = new ArrayList<MeasurementTask>();
     long totalduration=0;
@@ -172,6 +235,56 @@ public class SequentialTask extends MeasurementTask{
         if(stopFlag){
           throw new MeasurementError("Cancelled");
         }
+/*        
+        if(allResults.size()>0 && mt.getType().equals(TracerouteTask.TYPE) && ((TracerouteDesc)(mt.getDescription())).preCondition!=null){
+        	String precond=((TracerouteDesc)(mt.getDescription())).preCondition;
+        	Logger.d(">>>>>>>>>>>>>>> "+precond);
+        	PhoneUtils phoneUtils = PhoneUtils.getPhoneUtils();
+        	String network_type=phoneUtils.getNetwork().replace(" ", "").toLowerCase();
+        	
+        	float rttThreshold=-1;
+        	Type listType = new TypeToken<ArrayList<ArrayList<String>>>() {}.getType();
+        	List<ArrayList<String>> precondList = new Gson().fromJson(precond, listType);
+        	
+        	for(ArrayList<String> condition: precondList){
+        		if(condition.size()==3){//WiFi precondition
+        			String cond_nettype=condition.get(0);
+        			String cond_ssid=condition.get(1);
+        			float cond_rtt=Float.parseFloat(condition.get(2));
+        			if(cond_nettype.equals(network_type) && cond_ssid.equals(phoneUtils.getWifiSSID())
+        					&& cond_rtt>0){
+        				Logger.i("Precond matched: "+cond_nettype+" "+cond_ssid);
+        				rttThreshold=cond_rtt;
+        				break;
+        			}
+        			
+        			
+        		}else if(condition.size()==2){//cellular precondition
+        			String cond_nettype=condition.get(0);
+        			float cond_rtt=Float.parseFloat(condition.get(1));
+        			if(cond_nettype.equals(network_type) && cond_rtt>0){
+        				Logger.i("Precond matched: "+cond_nettype);
+        				rttThreshold=cond_rtt;
+        				break;
+        			}
+        		}
+        	}
+        	
+        	float pingRtt=-1;
+        	if(allResults.get(allResults.size()-1).getValues().get("min_rtt_ms")!=null){
+        		pingRtt=Float.parseFloat(allResults.get(allResults.size()-1).getValues().get("min_rtt_ms"));
+        	}
+        	
+        	if(rttThreshold==-1 || pingRtt==-1 || rttThreshold>pingRtt){
+        		Logger.i("Traceroute diag task is not going to run because: "+rttThreshold+" "+pingRtt);
+        		break;
+        	}else if(rttThreshold!=-1 && pingRtt!=-1){
+        		allResults.get(allResults.size()-1).getValues().put("rtt_thresh", rttThreshold+"");
+        	}
+        }
+        
+*/        
+        Logger.i("Sub task "+mt.getType()+" is going to run");
         Future<MeasurementResult[]> f=executor.submit(mt);
         currentTask=mt;
         MeasurementResult[] results;
